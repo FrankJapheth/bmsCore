7c506760b0735a554f40582259ee4e7d53af4058
Franklyne Adede <adedefranklyne@gmail.com>
2022-12-06 18:21:10 +0300

    cleared the way

--- bmsCore/settings.py
+++ bmsCore/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1']
+print((config('ALLOWED_HOSTS').split(',')))
+
+ALLOWED_HOSTS = ['127.0.0.1', ]
 
 # Application definition
 
